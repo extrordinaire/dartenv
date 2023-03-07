9d3f57e6698cbed05705eecf105fecfca046cb06
Maximo Angel Verzini Davico <maximoverzini@gmail.com>
2023-03-07 12:40:23 -0300

    First commit.

---
+++ example/dartenv_example.dart
@@ -0,0 +1,6 @@
+import 'package:dartenv/dartenv.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dartenv.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dartenv;
+
+export 'src/dartenv_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
